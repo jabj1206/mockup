d969441088c574dcf289221832d961e737b1c5ee
Jorge Betancur <jabetancur12@gmail.com>
2019-08-25 23:59:55 -0500

    Logo

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Filters from './Filters'
 import './styles/Sidebar.css'
+import logo from '../logo.svg';  
 
 
 export default class Sidebar extends Component {
     render() {
         return (
             <div className='text-center' style={{ color: 'gray', marginTop: '30px' }}>
-                <img src='https://abeon-hosting.com/images/react-js-png-10.png'
-                    alt=' ' style={{ width: '100px' }} />
+                <img src={logo} className="App-logo" alt="logo" 
+                     style={{ width: '100px' }} />
                 <Filters sortt = {this.props.sortt}/>
             </div>
         )
