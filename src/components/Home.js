73dd490b857c5294dc35dab71c35616f350e6431
Jorge Betancur <jabetancur12@gmail.com>
2019-08-28 20:21:49 -0500

    search origin-destiny

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,17 @@ import { Col } from "react-bootstrap";
 import Departures from "./Departures";
 
 class Home extends Component {
+  state = { search: "" };
+  _handleChange = e => {
+    this.setState({ search: e.target.value });
+  };
   render() {
+    const { search } = this.state;
+    const lowercasedFilter = search.toLowerCase();
+    const filteredData = this.props.depar.filter(item => {
+      return item['origin'].toLowerCase().includes(lowercasedFilter) || item['destiny'].toLowerCase().includes(lowercasedFilter)
+    });
+
     return (
       <>
         <br />
@@ -14,13 +24,15 @@ class Home extends Component {
               className="text-center"
               style={{ border: "none", borderRadius: "10px" }}
               placeholder="Search"
+              onChange={this._handleChange}
+              value={this.state.search}
             />
           </Col>
         </Row>
 
         <Row>
           <Col xs={12}>
-            <Departures depar={this.props.depar} deleteD={this.props.deleteD} />
+            <Departures depar={filteredData} deleteD={this.props.deleteD} />
           </Col>
         </Row>
       </>
